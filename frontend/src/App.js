983a8322c3c57db06ed407c93ee43a031f35b8a3
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2024-10-04 20:13:18 +0530

    Initial Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useClipboard from "react-use-clipboard";
+import toast,{Toaster} from 'react-hot-toast';
 
 function App() {
 
-  const [text, settext] = useState()
+  const [text, settext] = useState('')
   const {
     transcript,
     listening,
@@ -18,13 +19,28 @@ function App() {
     successDuration: 1000,
   });
 
-  useEffect(()=>{
+  useEffect(() => {
     settext(transcript)
-  },[transcript])
+  }, [transcript])
 
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  const copyfunc = () => {
+    setCopied();
+    toast.success("Copied to Clipboard");
+  }
+
+  const startfunc = () => {
+    SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+    toast.success("Microphone is now active. Please start speaking.");
+  }
+
+  const stopfunc = () => {
+    SpeechRecognition.stopListening();
+    toast.success("Microphone turned off. Voice input disabled.");
   }
 
   return (
@@ -34,18 +50,22 @@ function App() {
         <p><i>"Bridging the gap between speech and text seamlessly."</i></p>
 
         <div className='content-container'>
-          <p style={{color: '#D9F4C7'}}>Microphone: <i>{listening ? 'on' : 'off'}</i></p>
+          <p style={{ color: '#D9F4C7' }}>Microphone: <i>{listening ? 'on' : 'off'}</i></p>
           <div className='main-content'>
             {transcript}
           </div>
           <div className='buttons'>
-            <button onClick={setCopied}>{isCopied ? "Copied!" : "Copy"}</button>
-            <button onClick={()=>SpeechRecognition.startListening({ continuous: true, language: 'en-IN' })}>Start</button>
-            <button onClick={SpeechRecognition.stopListening}>Stop</button>
+            <button onClick={copyfunc}>{isCopied ? "Copied!" : "Copy"}</button>
+            <button onClick={startfunc}>Start</button>
+            <button onClick={stopfunc}>Stop</button>
             <button onClick={resetTranscript}>Reset</button>
           </div>
         </div>
       </div>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
     </>
   );
 }
